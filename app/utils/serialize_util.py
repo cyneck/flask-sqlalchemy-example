f416b10eeb2fb3913062f71b12e8d7588d3b244f
Eric Lee <cyneck@qq.com>
2020-06-26 16:04:27 +0800

    fix:只取模型属性

--- app/utils/serialize_util.py
+++ app/utils/serialize_util.py
@@ -16,13 +16,14 @@ class AlchemyJsonEncoder(json.JSONEncoder):
         if isinstance(obj.__class__, DeclarativeMeta):
             # an SQLAlchemy class
             fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+            for field in [x for x in dir(obj) if hasattr(obj, x) and not x.startswith('_') and x != 'metadata']:
                 data = obj.__getattribute__(field)
                 try:
                     json.dumps(data)  # this will fail on non-encodable values, like other classes
                     fields[field] = data
                 except TypeError:
-                    fields[field] = None
+                    # fields[field] = None
+                    pass
             # a json-encodable dict
             return fields
 
